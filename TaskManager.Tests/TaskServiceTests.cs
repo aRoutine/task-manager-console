304c8a05eb377cd0013fedb835f2e2a6579d06fd
Maxim Kovalev <maxim.kovalev2388@gmail.com>
2026-07-18 12:48:26 +0300

    Add unit tests for task service

--- TaskManager.Tests/TaskServiceTests.cs
+++ TaskManager.Tests/TaskServiceTests.cs
@@ -15,11 +15,112 @@ public class TaskServiceTests
         TaskService fakeService = new TaskService(fakeStorage);
 
         // Act
-        var result = fakeService.AddTask("Изучение английского языка", Models.TaskPriority.Medium);
+        var result = fakeService.AddTask("valid name", TaskPriority.Medium);
 
         // Assert
         Assert.True(result.Success);
         Assert.Equal("Задача успешно добавлена", result.Message);
         Assert.Equal(1, fakeStorage.SaveCallCount);
     }
+
+    [Fact]
+    public void AddTask_WithEmptyTitle_ShouldReturnFail()
+    {
+        //Arrange
+        FakeTaskStorage storage = new FakeTaskStorage();
+        TaskService service = new TaskService(storage);
+
+        //Act
+        var result = service.AddTask("", TaskPriority.Medium);
+
+        //Assert
+        Assert.False(result.Success);
+        Assert.Equal("Название задачи не может быть пустым", result.Message);
+        Assert.Equal(0, storage.SaveCallCount);
+    }
+
+    [Fact]
+    public void DeleteTask_WithUnknownId_ShouldReturnFalse()
+    {
+        //Arrange
+        FakeTaskStorage storage = new FakeTaskStorage();
+        TaskService service = new TaskService(storage);
+
+        //Act
+        var result = service.DeleteTask(3);
+
+        //Assert
+        Assert.False(result.Success);
+        Assert.Equal("Задача по заданному Id не была найдена в базе данных", result.Message);
+        Assert.Equal(0, storage.SaveCallCount);
+    }
+
+    [Fact]
+    public void CompleteTask_WithExistingTask_ShouldReturnSuccess()
+    {
+        //Arrange
+        FakeTaskStorage storage = new FakeTaskStorage();
+        TaskService service = new TaskService(storage);
+        service.AddTask("valid name", TaskPriority.Low);
+
+        //Act
+        var result = service.CompleteTask(1);
+
+        //Assert
+        Assert.True(result.Success);
+        Assert.Equal("Задача успешно выполнена", result.Message);
+    }
+
+    [Fact]
+    public void CompleteTask_WithAlreadyComplitedTask_ShouldReturnFail()
+    {
+        //Arrange
+        FakeTaskStorage storage = new FakeTaskStorage();
+        TaskService service = new TaskService(storage);
+        service.AddTask("valid name", TaskPriority.Low);
+        service.CompleteTask(1);
+
+        //Act
+        var result = service.CompleteTask(1);
+
+        //Assert
+        Assert.False(result.Success);
+        Assert.Equal("Задача уже выполнена", result.Message);
+    }
+
+    [Fact]
+    public void CompleteTask_WithUnknownId_ShouldReturnfail()
+    {
+        //Arrange
+        FakeTaskStorage storage = new FakeTaskStorage();
+        TaskService service = new TaskService(storage);
+
+        //Act
+        var result = service.CompleteTask(3);
+
+        //Assert
+        Assert.False(result.Success);
+        Assert.Equal("Задача по заданному ID не найдена", result.Message);
+    }
+
+    [Fact]
+    public void GetTasks_ShouldReturnTasksSortedByPriority()
+    {
+        //Arrange
+        FakeTaskStorage storage = new FakeTaskStorage();
+        TaskService service = new TaskService(storage);
+
+        service.AddTask("Важное задание", TaskPriority.High);
+        service.AddTask("Неважное задание", TaskPriority.Low);
+        service.AddTask("Обычное задание", TaskPriority.Medium);
+
+        //Act
+        var result = service.GetTasks();
+
+        //Assert
+        Assert.Equal(TaskPriority.High, result[0].TaskPriority);
+        Assert.Equal(TaskPriority.Medium, result[1].TaskPriority);
+        Assert.Equal(TaskPriority.Low, result[2].TaskPriority);
+    }
+
 }
