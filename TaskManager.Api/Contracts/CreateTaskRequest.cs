5236bf1e9580d0e3298e2683a09e2ea2f3439d9e
Maxim Kovalev <maxim.kovalev2388@gmail.com>
2026-07-22 13:54:43 +0300

    Add API request validation

--- TaskManager.Api/Contracts/CreateTaskRequest.cs
+++ TaskManager.Api/Contracts/CreateTaskRequest.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
 using TaskManager.Models;
 
 namespace TaskManager.Api.Contracts;
 
 public class CreateTaskRequest
 {
+    [Required(ErrorMessage = "Название задачи обязательно")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Название задачи должно быть от 3 до 100 символов")]
     public string Title { get; set; } = string.Empty;
+
+    [Range(1, 3, ErrorMessage = "Приоритет должен быть в диапазоне от 1 до 3")]
     public TaskPriority Priority { get; set; }
 }
--- TaskManager.Api/Contracts/RenameTaskRequest.cs
+++ TaskManager.Api/Contracts/RenameTaskRequest.cs
@@ -1,6 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TaskManager.Api.Contracts;
 
 public class RenameTaskRequest
 {
-    public string Title = string.Empty;    
+    [Required(ErrorMessage = "Название задачи обязательно")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Название задачи должно быть от 3 до 100 символов")]
+    public string Title = string.Empty;
 }
